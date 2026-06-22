e125612ffe7c25b85bf55ae4f2e65b7f41ff8412
MADHUSUSDAN <madhusudan8005@gmail.com>
2026-06-22 23:01:20 +0530

    Create indexes for candidates and interviews tables

---
+++ sql/18_indexes.sql
@@ -0,0 +1,3 @@
+create index idx_candidate_skill on candidates(primary_skill);
+create index idx_interview_score on interviews(final_score);
+create index idx_interview_date on interviews(interview_date);
