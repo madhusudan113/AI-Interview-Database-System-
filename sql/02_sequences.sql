f47aee8f4c16aa95fb91a137dddf5e55353a4a18
MADHUSUSDAN <madhusudan8005@gmail.com>
2026-06-22 22:39:06 +0530

    Create multiple sequences for database management

---
+++ sql/02_sequences.sql
@@ -0,0 +1,8 @@
+create sequence candidate_seq start with 1;
+create sequence interviewer_seq start with 100;
+create sequence interview_seq start with 1000;
+create sequence fraud_seq start with 1;
+create sequence notification_seq start with 1;
+create sequence audit_seq start with 1;
+create sequence skill_gap_seq start with 1;
+create sequence recommendation_seq start with 1;
